ba50b0046fc3919f8ed6b9ae1c59ee80434b0e63
kazukikanomata <kazu.surface.pro2020@outlook.jp>
2022-10-16 17:59:57 +0900

    add：モデル処理

---
+++ app/Models/Category.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    public function getLists(){
+        // id と nameのキーのみを取得する
+        $categories = Category::orderBy('id','asc')->pluck('name','id');
+        return $categories;
+    }
+    
+    public function tasks(){
+        return $this->hasMany('App\Models\Task');
+    }
+}
---
+++ app/Models/Task.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Task extends Model
+{
+    use SoftDeletes;
+
+    public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function category(){
+        return $this->belongsTo('App\Models\Category');
+    }
+    // 任意のカテゴリを含むものとする（ローカル）スコープ
+    public function scopeCategoryAt($query, $category_id){
+    if (empty($category_id)) {
+        return;
+    }
+ 
+    return $query->where('category_id', $category_id);
+    }
+    
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'content',
+        'due_time',
+        'status',
+        'time',
+        ];
+    protected $dates = ['due_time'];
+    protected $table = 'tasks';
+}
