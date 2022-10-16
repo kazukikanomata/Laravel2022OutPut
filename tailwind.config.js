52d61de730b164cc1ca526cec1be5d68e3025fe9
kazukikanomata <kazu.surface.pro2020@outlook.jp>
2022-10-16 16:50:37 +0900

    add：tailwindcss in content

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/tasks/top.blade.php',
+        './resources/views/tasks/create.blade.php',
+        './resources/views/tasks/edit.blade.php',
+        './resources/views/tasks/index.blade.php',
+        './resources/views/tasks/show.blade.php',
     ],
 
     theme: {
